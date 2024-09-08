8a39a02891e260611a86f3409c6c77dfa533ed8f
mxdpills <angelstlop25@gmail.com>
2024-09-07 21:53:46 -0500

    segundo push

---
+++ ejercicio 1/js/dataCursos.js
@@ -0,0 +1,15 @@
+const cursosAsignados = [
+    {
+        Nombre_curso: '',
+        Horario: '',
+        Fecha: '',
+        Tipo_clase: '',
+        Periodo_academico:'',
+        Sección: '',
+        Codigo_alumno: '',
+        Codigo_docente: ''
+    },
+    {
+        
+    }
+]
